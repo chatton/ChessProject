14c9cf91d1c3b9862a3e39e6eb771b178a59ccfc
chatton <cianhatton@gmail.com>
2018-01-31 12:19:56 +0000

    Initial commit

---
+++ src/main/java/ie/gmit/sw/ChessProjectApplication.java
@@ -0,0 +1,12 @@
+package ie.gmit.sw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessProjectApplication.class, args);
+	}
+}
---
+++ src/main/java/ie/gmit/sw/controllers/ChessController.java
@@ -0,0 +1,13 @@
+package ie.gmit.sw.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChessController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "<h1>Hi!</h1>";
+    }
+}
