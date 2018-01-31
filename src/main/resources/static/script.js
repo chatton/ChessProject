9e967861d63c259e43a15e1f2bc346bf0dbf5efa
chatton <cianhatton@gmail.com>
2018-01-31 17:48:16 +0000

    By default Spring boot serves files from resources/static. Added index.html and script.js that will contain the client chess app.

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "red";
+ctx.fillRect(100,100,100,100);
+
+alert("The Javascript, it works!");
