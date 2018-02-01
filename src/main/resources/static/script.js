250275cf223142e197d71a6590df97ab09fe6e1f
DamianGavin <10007969@gmit.ie>
2018-02-01 20:47:24 +0000

    Chess board designed on Html5 canvas, with x-y click

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,7 +1,49 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+//<script type="text/javascript">
+// Gets a handle to the element with id canvasOne.
+canvas = document.getElementById("Chess");
+// Set the canvas up for drawing in 2D.
+ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "red";
-ctx.fillRect(100,100,100,100);
+function getMousePos(canvas, evt)
+{
+    var rect = canvas.getBoundingClientRect();
+    return{
+        x:evt.clientX - rect.left,
+        y:evt.clientY - rect.top
+    };
+}
 
-alert("The Javascript, it works!");
+
+function handleClick(e)
+{
+
+}
+canvas.addEventListener("click", function (e) {
+    console.log("clicked!");
+    var pos = getMousePos(canvas, e);
+    var posx = pos.x;
+    var posy = pos.y;
+
+    alert(posx + " " + posy);
+});
+
+function draw()
+{
+    for(var x=0; x < 8; x++)
+    {
+        for(var y=0; y < 8; y++)
+        {
+            if (x%2 === y%2)
+            {
+                ctx.fillStyle = "black";
+                ctx.fillRect(63*x, 63*y, 63, 63);
+            }
+            else
+            {
+                ctx.fillStyle = "white";
+                ctx.fillRect(63*x, 63*y, 63, 63);
+            }
+        }
+    }
+}
+draw();
