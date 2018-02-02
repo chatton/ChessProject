9a9cf9d872b7e83a3eeff2bbe82e45965172368a
majo-z <g00340481@gmit.ie>
2018-02-02 13:05:21 +0000

    add pawn logic

--- src/main/java/ie/gmit/sw/chess/board/pieces/Pawn.java
+++ src/main/java/ie/gmit/sw/chess/board/pieces/Pawn.java
@@ -2,8 +2,8 @@ package ie.gmit.sw.chess.board.pieces;
 
 import ie.gmit.sw.chess.board.ChessBoard;
 import ie.gmit.sw.chess.board.Position;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Pawn extends Piece {
@@ -13,6 +13,63 @@ public class Pawn extends Piece {
 
     @Override
     public List<Position> getPossiblePositions() {
-        throw new NotImplementedException();
+        List<Position> validPositions = new ArrayList<>();
+        if (colour == Colour.WHITE) {
+            // get the position directly in front
+            Position inFront = new Position(position.x(), position.y() - 1);
+            addIfFrontPositionValid(validPositions, inFront);
+
+            Position[] diagonals = {
+                    new Position(position.x() + 1, position.y() - 1),
+                    new Position(position.x() - 1, position.y() - 1)
+            };
+            // add these to validPositions
+            addIfDiagonalsValid(validPositions, diagonals);
+
+        } // else for BLACK pieces
+        else {
+            // the piece is black
+            Position inFront = new Position(position.x(), position.y() + 1);
+            addIfFrontPositionValid(validPositions, inFront);
+
+            Position[] diagonals = {
+                    new Position(position.x() + 1, position.y() + 1),
+                    new Position(position.x() - 1, position.y() + 1)
+            };
+            // add these to validPositions
+            addIfDiagonalsValid(validPositions, diagonals);
+        }//else
+
+        return validPositions;
+
+    }
+
+    private void addIfFrontPositionValid(List<Position> positions, Position pos) {
+        if (!board.isOnBoard(pos)) {
+            return;
+        }
+        // check if it's empty
+        if (board.posIsEmpty(pos)) {
+            positions.add(pos);
+        }
+    }
+
+    private void addIfDiagonalsValid(List<Position> validPositions, Position[] positions) {
+        for (Position pos : positions) {
+            if (!board.isOnBoard(pos)) {
+                continue;//look at every position in the array
+            }
+            // check if it's empty
+            if (board.posIsEmpty(pos)) {
+                continue;
+            }
+            //check if there is an enemy piece
+            Piece piece = board.getAt(pos);
+            if (piece.colour != colour) {
+                validPositions.add(pos);
+            }
+
+        }
+
     }
 }
