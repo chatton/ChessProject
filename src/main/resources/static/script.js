ad36faa1280cea2973aa52fca873a39ee26e0838
chatton <cianhatton@gmail.com>
2018-02-03 11:36:50 +0000

    in order to be hosted, images needed to be moved to /resources/static/images
    HTML canvas now draws a rectangle and then an image over it. current image
    is hard coded as bishop and needs to be changed depending on the board state.

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -26,19 +26,33 @@ function draw() {
     for (let x = 0; x < BOARD_SIZE; x++) {
         for (let y = 0; y < BOARD_SIZE; y++) {
             if (x % 2 === y % 2) {
-                ctx.fillStyle = "black";
-                ctx.fillRect(GRID_SIZE * x, GRID_SIZE * y, GRID_SIZE, GRID_SIZE);
+
                 // Draw image here
+                const image = new Image();
+                image.onload = () => { // run this function when the image fully loads
+                    ctx.fillStyle = "black"; // draw the rectangle underneath
+                    ctx.fillRect(GRID_SIZE * x, GRID_SIZE * y, GRID_SIZE, GRID_SIZE);
+                    // then draw the image.
+                    ctx.drawImage(image, GRID_SIZE * x, GRID_SIZE * y, GRID_SIZE, GRID_SIZE);
+                };
+                image.src= "images/bBishop.png"; // TODO don't hard code all bishops!
             }
             else {
-                ctx.fillStyle = "white";
-                ctx.fillRect(GRID_SIZE * x, GRID_SIZE * y, GRID_SIZE, GRID_SIZE);
+
                 // Draw image here
+                const image = new Image();
+                image.src= "images/bBishop.png"; // remove hard coded path - need to determine based on position.
+                image.onload = () => {
+                    ctx.fillStyle = "white";
+                    ctx.fillRect(GRID_SIZE * x, GRID_SIZE * y, GRID_SIZE, GRID_SIZE);
+                    ctx.drawImage(image, GRID_SIZE * x, GRID_SIZE * y, GRID_SIZE, GRID_SIZE);
+                };
+
             }
         }
     }
 }
-function start(){
+function start() {
     draw();
     window.requestAnimationFrame(start);
 }
