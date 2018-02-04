6c032c76b6b9b321a2cba9f1e88ffb126583c1c0
chatton <cianhatton@gmail.com>
2018-02-04 22:01:55 +0000

    Added requests library import and test GET request.

--- src/main/python/chess-bot/chess-bot.py
+++ src/main/python/chess-bot/chess-bot.py
@@ -1,12 +1,21 @@
 from pieces import *
 from game import *
 
+# import urllib.request
+
+import requests
+
 def main():
     board = Board()
     
     board.set_piece( Bishop(board,Colour.WHITE), (3,3))
     k = board.get_piece((3,3))
     print(k.moves)
+    url = "http://localhost:8080/test"
+    # f = urllib.request.urlopen(url)
+    # print(f.read().decode("utf-8"))
+    r = requests.get(url)
+    print(r.json())
 
 if __name__ == "__main__":
     main()
