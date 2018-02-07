6fe78dfd0458c8543945980613fe0d426640efcf
chatton <cianhatton@gmail.com>
2018-02-07 23:49:57 +0000

    added ability to specify a port for the server to run on as a command line argument.

--- src/main/java/ie/gmit/sw/ChessProjectApplication.java
+++ src/main/java/ie/gmit/sw/ChessProjectApplication.java
@@ -1,12 +1,33 @@
 package ie.gmit.sw;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class ChessProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChessProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
+        int port = 8080; // by default use port 8080
+        if (args.length > 0) { // specific port provided by user.
+            try {
+                port = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                System.out.println("Must specify a valid number for port. Exiting...");
+                return;
+            }
+        }
+        System.out.println(port);
+        final Map<String, Object> props = new HashMap<>();
+        props.put("server.port", port);
+
+        new SpringApplicationBuilder()
+                .sources(ChessProjectApplication.class)
+                .properties(props)
+                .run(args);
+    }
 }
