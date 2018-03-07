08e6ef2fb037abb02a5a1f8ee45eb0980c085771
chatton <cianhatton@gmail.com>
2018-03-07 19:01:59 +0000

    Added move converter, required to save move object into db

---
+++ src/main/java/ie/gmit/sw/chess/board/MoveConverter.java
@@ -0,0 +1,27 @@
+package ie.gmit.sw.chess.board;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+
+@Converter
+public class MoveConverter implements AttributeConverter<Move, String> {
+
+
+    @Override
+    public String convertToDatabaseColumn(Move move) {
+        if(move == null){
+            return null;
+        }
+        return move.to().toChess() +"_" + move.from().toChess();
+    }
+
+    @Override
+    public Move convertToEntityAttribute(String s) {
+        if(s == null){
+            return null;
+        }
+        String[] fromTo = s.split("_");
+        return new Move(fromTo[0], fromTo[1]);
+    }
+}
